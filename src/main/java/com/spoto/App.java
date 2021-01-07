efaddeb8e13d3c0707bc190837f6a5b4597c0a62
spoto-dl <980816303@qq.com>
2021-01-07 20:55:50 +0800

    新建的项目

---
+++ src/main/java/com/spoto/App.java
@@ -0,0 +1,13 @@
+package com.spoto;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
